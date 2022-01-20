33aba346984f08a31bb1bc785527c8c78339d9f4
Nguyễn Trung Hiếu <17520480@gm.uit.edu.vn>
2022-01-20 13:51:55 +0700

    ! Add some conditional statement

--- src/app/models/PKTT.model.ts
+++ src/app/models/PKTT.model.ts
@@ -19,57 +19,11 @@ export interface PKTT {
     OffsetDuoi:number,
 }
 
-// Firestore data converter
-// export const PKConverter = {
-//     toFirestore: function(pktt: PKTT) {
-//         return {
-//             Ma:     pktt.Ma,
-//             Ten:    pktt.Ten,
-//             Loai:   pktt.Loai,
-//             Hinh:   pktt.Hinh,
-//             MoTa:   pktt.MoTa,
-//             Hang:   pktt.Hang,
-//             Mau:    pktt.Mau,
-//             Gia:    pktt.Gia,
-//             SoLuong:pktt.SoLuong,
-
-//             //for ar
-//             OffsetTrai:pktt.OffsetTrai,
-//             OffsetPhai:pktt.OffsetPhai,
-//             OffsetTren:pktt.OffsetTren,
-//             OffsetDuoi:pktt.OffsetDuoi,
-//             };
-//     },
-    
-//     fromFirestore: function(snapshot, options): PKTT{
-//         const pktt = snapshot.data(options);
-//         const kq: PKTT = {
-//             Ma:     pktt.Ma,
-//             Ten:    pktt.Ten,
-//             Loai:   pktt.Loai,
-//             Hinh:   pktt.Hinh,
-//             MoTa:   pktt.MoTa,
-//             Hang:   pktt.Hang,
-//             Mau:    pktt.Mau,
-//             Gia:    pktt.Gia,
-//             SoLuong:pktt.SoLuong,
-//             PhanLoai: pktt.PhanLoai,
-            
-//             //for ar
-//             OffsetTrai: pktt.OffsetTrai,
-//             OffsetPhai: pktt.OffsetPhai,
-//             OffsetTren: pktt.OffsetTren,
-//             OffsetDuoi: pktt.OffsetDuoi,
-//         };
-//         return kq;
-//     }
-// }
-
 export const loaiOptions: string[] = ["Hat", "Shirt", "Pants", "Backpack", "Shoes"];
 export const phanLoaiOptions:ESMap<string, string[]> = new Map([
     [loaiOptions[0], ['Caps', 'Beanies', 'Fedoras', 'Berets']],
     [loaiOptions[1], ['Shirt', 'T-Shirt', 'Jacket']],
-    [loaiOptions[2], ['Jeans', 'Shorts', 'Skirt', 'Kaki']],
+    [loaiOptions[2], ['Jeans', 'Shorts', 'Short skirts', 'Long skirts', 'Kaki']],
     [loaiOptions[3], ['Backpack']],
     [loaiOptions[4], ['Shoes', 'Slipper']],
 ]);
